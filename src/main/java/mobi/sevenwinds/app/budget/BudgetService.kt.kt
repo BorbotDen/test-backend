2f919256c6293fe003a4079a6821aafc27cbea7c
Denis Borbot <Borbot@mail.ru>
2023-09-26 22:08:00 +0400

    add author name and dateTime on stats

--- src/main/java/mobi/sevenwinds/app/budget/BudgetService.kt.kt
+++ src/main/java/mobi/sevenwinds/app/budget/BudgetService.kt.kt
@@ -5,6 +5,7 @@ import kotlinx.coroutines.withContext
 import mobi.sevenwinds.app.author.AuthorEntity
 import mobi.sevenwinds.app.author.AuthorTable
 import org.jetbrains.exposed.dao.EntityID
+import org.jetbrains.exposed.sql.innerJoin
 import org.jetbrains.exposed.sql.select
 import org.jetbrains.exposed.sql.transactions.transaction
 import org.joda.time.DateTime
@@ -23,7 +24,7 @@ object BudgetService {
                 this.month = body.month
                 this.amount = body.amount
                 this.type = body.type
-                this.authorId = body.authorId?.let { EntityID(it, AuthorTable)}!!
+                this.authorId = body.authorId?.let { EntityID(it, AuthorTable) }!!
             }
 
             return@transaction entity.toResponse()
@@ -33,15 +34,52 @@ object BudgetService {
     suspend fun getYearStats(param: BudgetYearParam): BudgetYearStatsResponse = withContext(Dispatchers.IO) {
         transaction {
             val query = BudgetTable
+                .innerJoin(AuthorTable, { authorId }, { AuthorTable.id })
                 .select { BudgetTable.year eq param.year }
                 .limit(param.limit, param.offset)
 
             val total = query.count()
-            val data = BudgetEntity.wrapRows(query)
-                .sortedWith(compareBy<BudgetEntity> {it.month}.thenByDescending {it.amount})
-                .map { it.toResponse() }
+            val data = query
+                .orderBy(BudgetTable.month)
+                .map {
+                    val authorName = it[AuthorTable.name]
+                    if (authorName != null) {
+                        BudgetRecordWithAuthor(
+                            year = it[BudgetTable.year],
+                            month = it[BudgetTable.month],
+                            amount = it[BudgetTable.amount],
+                            type = it[BudgetTable.type],
+                            authorId = it[BudgetTable.authorId]?.value,
+                            authorName = authorName,
+                            authorCreatedDt = it[AuthorTable.createdDt].toString()
+                        )
+                    } else {
+                        BudgetRecord(
+                            year = it[BudgetTable.year],
+                            month = it[BudgetTable.month],
+                            amount = it[BudgetTable.amount],
+                            type = it[BudgetTable.type],
+                            authorId = it[BudgetTable.authorId]?.value
+                        )
+                    }
+                }
 
-            val sumByType = data.groupBy { it.type.name }.mapValues { it.value.sumOf { v -> v.amount } }
+            val sumByType = data.groupBy {
+                when (it) {
+                    is BudgetRecordWithAuthor -> it.type.toString()
+                    is BudgetRecord -> it.type.toString()
+                    else -> throw IllegalArgumentException("Unknown type: ${it::class.simpleName}")
+                }
+
+            }.mapValues {
+                it.value.sumOf {
+                    when (it) {
+                        is BudgetRecordWithAuthor -> it.amount
+                        is BudgetRecord -> it.amount
+                        else -> throw IllegalArgumentException("Unknown type: ${it::class.simpleName}")
+                    }
+                }
+            }
 
             return@transaction BudgetYearStatsResponse(
                 total = total,
