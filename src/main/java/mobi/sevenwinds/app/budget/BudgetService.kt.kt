4301d970de9d317409e0ca863b1ef198b522c45e
Denis Borbot <Borbot@mail.ru>
2023-09-27 00:02:40 +0400

    add optional filter on query parameters

--- src/main/java/mobi/sevenwinds/app/budget/BudgetService.kt.kt
+++ src/main/java/mobi/sevenwinds/app/budget/BudgetService.kt.kt
@@ -5,7 +5,9 @@ import kotlinx.coroutines.withContext
 import mobi.sevenwinds.app.author.AuthorEntity
 import mobi.sevenwinds.app.author.AuthorTable
 import org.jetbrains.exposed.dao.EntityID
+import org.jetbrains.exposed.sql.andWhere
 import org.jetbrains.exposed.sql.innerJoin
+import org.jetbrains.exposed.sql.lowerCase
 import org.jetbrains.exposed.sql.select
 import org.jetbrains.exposed.sql.transactions.transaction
 import org.joda.time.DateTime
@@ -33,11 +35,14 @@ object BudgetService {
 
     suspend fun getYearStats(param: BudgetYearParam): BudgetYearStatsResponse = withContext(Dispatchers.IO) {
         transaction {
-            val query = BudgetTable
+            var query = BudgetTable
                 .innerJoin(AuthorTable, { authorId }, { AuthorTable.id })
                 .select { BudgetTable.year eq param.year }
                 .limit(param.limit, param.offset)
 
+            if (param.authorName != null) {
+                query = query.andWhere { AuthorTable.name.lowerCase() like "%${param.authorName.toLowerCase()}%" }
+            }
             val total = query.count()
             val data = query
                 .orderBy(BudgetTable.month)
